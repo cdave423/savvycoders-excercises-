70f6a7a4513f8ae7d24bbb2bee4f34b1f3e4a438
Chris Dave <cdave3741@gmail.com>
2017-03-18 11:06:50 -0700

    rps

---
+++ js/main.js
@@ -0,0 +1,16 @@
+alert("hello");
+
+var name = "chris";
+
+function checkuser (username) {
+
+  if (username=="chris") {
+
+    console.log("Welcome, Authenticated user.");
+  }
+    else {
+      console.log("Access Denied, Termination eminate.");
+    }
+
+  }
+  checkuser(name);
